30fa9b857adfb1ecd6f95083339ee35ed0676231
Kevin Deffo <deffo.codevigor@gmail.com>
2025-06-19 23:47:48 +0100

    solve style issue

--- app.py
+++ app.py
@@ -1,171 +1,5 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, redirect, url_for, flash, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
-from datetime import datetime
-from forms import RegisterForm, LoginForm, PostForm, CommentForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2001@localhost/test'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-
-# ---------------- Models ----------------
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    comments = db.relationship('Comment', backref='user', lazy=True)
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comments = db.relationship('Comment', backref='post', lazy=True)
-
-
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
-# ---------------- Login Loader ----------------
-
-@login_manager.user_loader
-def load_user(user_id):
-    return db.session.get(User, int(user_id))  # Modern SQLAlchemy 2.x compatible
-
-
-# ---------------- Routes ----------------
-
-@app.route('/')
-def home():
-    posts = Post.query.order_by(Post.date.desc()).all()
-    return render_template('home.html', posts=posts)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, password=hashed_pw)
-        db.session.add(user)
-        db.session.commit()
-        flash('Account created. You can now log in.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            flash('Login successful.', 'success')
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
-        flash('Invalid credentials.', 'danger')
-    return render_template('login.html', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You have been logged out.', 'info')
-    return redirect(url_for('home'))
-
-
-@app.route('/create', methods=['GET', 'POST'])
-@login_required
-def create():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Post created.', 'success')
-        return redirect(url_for('home'))
-    return render_template('create_post.html', form=form)
-
-
-@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
-def view_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    form = CommentForm()
-    if form.validate_on_submit() and current_user.is_authenticated:
-        comment = Comment(content=form.content.data, post=post, user=current_user)
-        db.session.add(comment)
-        db.session.commit()
-        flash('Comment added!', 'success')
-        return redirect(url_for('view_post', post_id=post.id))
-    return render_template('view_post.html', post=post, form=form)
-
-
-@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
-@login_required
-def edit(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        flash('Not authorized.', 'danger')
-        return redirect(url_for('home'))
-    form = PostForm(obj=post)
-    if form.validate_on_submit():
-        post.title = form.title.data
-        post.content = form.content.data
-        db.session.commit()
-        flash('Post updated.', 'success')
-        return redirect(url_for('view_post', post_id=post.id))
-    return render_template('edit_post.html', form=form)
-
-
-@app.route('/delete/<int:post_id>', methods=['POST'])
-@login_required
-def delete(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        flash('Not authorized.', 'danger')
-        return redirect(url_for('home'))
-    db.session.delete(post)
-    db.session.commit()
-    flash('Post deleted.', 'success')
-    return redirect(url_for('home'))
-
-
-@app.route('/myposts')
-@login_required
-def myposts():
-    posts = Post.query.filter_by(user_id=current_user.id).order_by(Post.date.desc()).all()
-    return render_template('myposts.html', posts=posts)
-
-
-# ---------------- App Runner ----------------
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
-=======
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-from flask_sqlalchemy import SQLAlchemy  
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm, CSRFProtect
 from flask_talisman import Talisman
@@ -175,7 +9,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import logging
 import secrets
+from dotenv import load_dotenv
 from config import Config
+
+load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -548,7 +385,5 @@ def view_post(post_id):
     return render_template('view_post.html', post=post, comments=comments, form=form)
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
->>>>>>> cff6260 (il s'agit d'un forum des passionnes de la cybersecurite et de la cryptographie, nous somme en phase de developpement l'interface frontend est pratiquement fini, sauf les fonctionnalites creer un post, commenter un post)
